98545aec178af228d40f23c7d661e381a54c1813
rlotr02 <0314kelee@naver.com>
2024-03-20 14:09:45 +0900

    Feat: button 제작, StartPage 제작

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes, css } from 'styled-components';
 
 interface TButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
+  onAni: boolean;
 }
 
-const Button: React.FC<TButtonProps> = ({ text, ...props }) => {
-  return <Btn {...props}>{text}</Btn>;
+const Button: React.FC<TButtonProps> = ({ text, onAni, ...props }) => {
+  const [speed, setSpeed] = useState(1);
+
+  useEffect(() => {
+    const randomSpeed = Math.random() * 5 + 1;
+    setSpeed(randomSpeed);
+  }, []);
+
+  return (
+    <Btn {...props} speed={speed} ani={onAni}>
+      {text}
+    </Btn>
+  );
 };
 
 export default Button;
 
-const Btn = styled.button`
+const blinkAnimation = keyframes`
+  50% {
+    opacity: 0;
+  }
+`;
+
+const Btn = styled.button<{ speed: number; ani: boolean }>`
   width: 228px;
   height: 58px;
   background-color: #a63333;
   font-family: 'Jeju Hallasan';
+  border: none;
   font-size: 20px;
   color: #000000;
 
   transition: all 0.2s ease;
+  ${(props) =>
+    props.ani &&
+    css`
+      animation: ${blinkAnimation} ${props.speed}s linear infinite;
+    `}
 
   @media (hover: hover) and (pointer: fine) {
     &:hover {
--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -3,19 +3,25 @@ import styled from 'styled-components';
 import { useSetRecoilState } from 'recoil';
 import { pageState } from '../assets/recoil';
 import Button from '../components/Button';
+import Background from '../assets/images/bg.png';
 
 const StartPage: React.FC = () => {
   const setPage = useSetRecoilState(pageState);
 
   return (
     <Container>
+      <img src={Background} alt="배경" />
       <div>
         <h1>
           HORROR
           <br />
           TEST
         </h1>
-        <Button text="테스트 시작하기" onClick={() => setPage(1)} />
+        <Button
+          text="테스트 시작하기"
+          onAni={true}
+          onClick={() => setPage(1)}
+        />
       </div>
     </Container>
   );
@@ -34,6 +40,22 @@ const Container = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
+
+  > img {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: -1;
+    width: 100%;
+    height: auto;
+
+    @media screen and (min-width: 750px) {
+      width: 750px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
 
   > div {
     @media screen and (min-width: 750px) {
