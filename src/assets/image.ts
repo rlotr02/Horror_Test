85c3270e43cf5267483f1b53e605fe0c7a420ef1
rlotr02 <0314kelee@naver.com>
2024-03-20 14:19:01 +0900

    Feat: 질문, 답변 api 추가

---
+++ src/assets/image.ts
@@ -0,0 +1,6 @@
+import Result1 from '../assets/images/용감한탐험가.png';
+import Result2 from '../assets/images/신중한관찰자.png';
+import Result3 from '../assets/images/조심스러운회피자.png';
+import Result4 from '../assets/images/독립적인해결사.png';
+
+export const RESULT_IMAGES = [Result1, Result2, Result3, Result4];
